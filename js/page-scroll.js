df8b8fb083873eb776858c3f64553572e1cf4180
Nguyên Vũ <vunguyen2472001@gmail.com>
2021-08-30 22:03:59 +0700

    front-end done

---
+++ js/page-scroll.js
@@ -0,0 +1,24 @@
+new fullScroll({
+
+    // parent container
+    container : 'main',
+
+    // content section
+    sections : 'section',
+
+    // animation speed
+    animateTime : 0.7,
+
+    // easing for animation
+    animateFunction : 'ease',
+
+    // current position
+    currentPosition: 0,
+
+    // display dots navigation
+    displayDots: true,
+
+    // where to place the dots navigation
+    dotsPosition: 'left'
+
+});
